b43ebd1918f0882443b1e2d849671f0b0ffc01b9
limonadeht <1212@gmail.com>
2015-08-23 15:02:25 +0900

    commit

--- java/lychemon/advtorch/AdvancedTorchTab.java
+++ java/lychemon/advtorch/AdvancedTorchTab.java
@@ -11,8 +11,7 @@ public class AdvancedTorchTab extends CreativeTabs
 		super(label);
 	}
 
-	//ここでタブのアイコンを返している。
-	/*アイテムの場合はそのままわたせばよいが、ブロックの場合はItemStackのgetItemメソッドを利用する。*/
+	//return the tabIcon
 	@Override
 	public Item getTabIconItem() {
 		return new ItemStack(Items.apple, 1, 0).getItem();
